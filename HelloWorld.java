f9f388032f88169d75974f2b1af04501a9e3f830
mojzno <norbert.mojzes@gmail.com>
2019-01-24 10:08:43 +0100

    fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld.java {
-    public static void main(Strin[] args) {
+public class HelloWorld {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
